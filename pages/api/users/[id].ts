72ce0e958316b4f1c73410b89ca2f35904851453
James Donnelly <hello@jamesdonnelly.dev>
2022-08-21 22:45:50 +0100

    feat: add user model with api endpoint

---
+++ pages/api/users/[id].ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  //   const newUser = await prisma.user.create({
+  //     data: {
+  //       name: "Alice",
+  //       email: "alice@prisma.io",
+  //     },
+  //   });
+
+  const users = await prisma.user.findMany();
+  res.status(200).json(users);
+}
